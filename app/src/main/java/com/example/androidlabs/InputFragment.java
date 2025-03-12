acb4eaa98e612d57791fccd7717139c5d6d5f559
Cl1mzzz <vborodiy2004@gmail.com>
2025-03-12 22:55:08 +0200

    lab2

---
+++ app/src/main/java/com/example/androidlabs/InputFragment.java
@@ -0,0 +1,63 @@
+package com.example.androidlabs;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class InputFragment extends Fragment {
+
+    private EditText nameEditText;
+    private RadioGroup colorGroup, priceGroup;
+    private Button orderButton;
+    private OnDataPassListener dataPasser;
+
+    public interface OnDataPassListener {
+        void onDataPass(String data);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        dataPasser = (OnDataPassListener) context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_input, container, false);
+
+        nameEditText = view.findViewById(R.id.editTextName);
+        colorGroup = view.findViewById(R.id.radioGroupColor);
+        priceGroup = view.findViewById(R.id.radioGroupPrice);
+        orderButton = view.findViewById(R.id.buttonOrder);
+
+        orderButton.setOnClickListener(v -> {
+            String name = nameEditText.getText().toString();
+            int colorId = colorGroup.getCheckedRadioButtonId();
+            int priceId = priceGroup.getCheckedRadioButtonId();
+
+            if (TextUtils.isEmpty(name) || colorId == -1 || priceId == -1) {
+                Toast.makeText(getContext(), R.string.error, Toast.LENGTH_SHORT).show();
+            } else {
+                RadioButton selectedColor = view.findViewById(colorId);
+                RadioButton selectedPrice = view.findViewById(priceId);
+                String result = "Ім'я: " + name + "\nКолір: " + selectedColor.getText() + "\nЦіна: " + selectedPrice.getText();
+                dataPasser.onDataPass(result);
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/androidlabs/ResultFragment.java
@@ -0,0 +1,46 @@
+package com.example.androidlabs;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultFragment extends Fragment {
+
+    private static final String ARG_RESULT = "result";
+
+    public static ResultFragment newInstance(String result) {
+        ResultFragment fragment = new ResultFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_RESULT, result);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
+
+        TextView resultTextView = view.findViewById(R.id.textViewResult);
+        Button cancelButton = view.findViewById(R.id.buttonCancel);
+
+        if (getArguments() != null) {
+            String result = getArguments().getString(ARG_RESULT);
+            resultTextView.setText(result);
+        }
+
+        cancelButton.setOnClickListener(v -> {
+            ((MainActivity) getActivity()).clearForm();
+        });
+
+        return view;
+    }
+}
